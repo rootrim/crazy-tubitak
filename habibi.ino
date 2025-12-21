e0a89ce5fc7e13c61cd26fc4d54207b13d25afc6
nixtrim <omuaz01@gmail.com>
2025-12-21 22:48:23 +0300

    more habbab

--- habibi.ino
+++ habibi.ino
@@ -6,12 +6,7 @@ LoRa_E32 e32ttl(&lora);
 
 void setup() {
   Serial.begin(9600); 
-
   e32ttl.begin();
-
-  ResponseStatus rs = e32ttl.sendFixedMessage(0, 3, 38, "Hello, LoRa!");
-
-  Serial.println(rs.getResponseDescription());
 }
 
 void loop() {
